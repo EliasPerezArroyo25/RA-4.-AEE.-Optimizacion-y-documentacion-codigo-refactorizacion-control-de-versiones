083e41ff52d0a50e975a9347cf466411accaac86
EliasPerezArroyo25 <eliasperez.25@campuscamara.es>
2026-05-19 14:33:51 +0200

    Reestructuracion código, Adición comentarios y documentación

--- src/facturacion/FacturacionLegacy.java
+++ src/facturacion/FacturacionLegacy.java
@@ -1,30 +1,44 @@
 package facturacion;
+
 /**
- * Clase Legacy del sistema de facturación.
- * ADVERTENCIA: Código con alta deuda técnica. No modificar la firma del método.
+ * Clase Legacy del sistema de facturación.Comprueba el descuento a aplicar
+ * según el tipo de Cliente
  */
 public class FacturacionLegacy {
-   
-    // Método a refactorizar
-    public double calcularTotal(double importeBase, int tipoCliente, boolean esSocioVip) {
-        if (importeBase > 0) {
-            if (tipoCliente == 1) {
-                if (esSocioVip == true)
-                    return importeBase - (importeBase * DESCUENTO_VIP_EXTRA);
-                else
-                    return importeBase - (importeBase * DESCUENTO_VIP);
-            } else {
-                if (tipoCliente == 2) {
-                    return importeBase - (importeBase * DESCUENTO_ESTANDAR);
-                } else {
-                    return importeBase;
-                }
-            }
-        } else {
-            return 0;
-        }
-    }
+    //Constantes
+    private static final int CLIENTE_SOCIO = 2;
+    private static final int CLIENTE_VIP = 1;
     private static final double DESCUENTO_ESTANDAR = 0.05;
     private static final double DESCUENTO_VIP = 0.15;
     private static final double DESCUENTO_VIP_EXTRA = 0.25;
+    // Método refactorizado
+    /**
+     * 
+     * @param importeBase El importe introducido
+     * @param tipoCliente El valor que tiene cada tipo de cliente
+     * @param esSocioVipExtra La afirmación de si es VIP o VIP_EXTRA
+     * @return Devuelve el importe con el descuento aplicado dependiendo del 
+     * tipo de cliente: VIP,VIP_EXTRA,SOCIO,CLIENTE
+     */
+    public double calcularTotal(double importeBase, int tipoCliente, boolean esSocioVipExtra) {
+        //Comprueba si es VIP    
+        if (tipoCliente == CLIENTE_VIP) {
+            //Comprueba si es VIP con EXTRA
+            if (esSocioVipExtra == true) {
+                //Devuelve con el descuento VIP_EXTRA aplicado
+                return importeBase - (importeBase * DESCUENTO_VIP_EXTRA);
+            }
+            //Devuelve con el descuento VIP aplicado
+            return importeBase - (importeBase * DESCUENTO_VIP);
+        }
+        //Comprueba si es SOCIO  
+        if (tipoCliente == CLIENTE_SOCIO) {
+            //Devuelve con el descuento SOCIO aplicado 
+            return importeBase - (importeBase * DESCUENTO_ESTANDAR);
+        }
+        //Devuelve sin aplicar descuento si es CLIENTE sin más
+        return importeBase;
+
+    }
+
 }
